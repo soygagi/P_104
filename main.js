e89537cf351ba81910c8c03952eb578a78b7328d
soygagi <101847092+soygagi@users.noreply.github.com>
2022-08-30 15:35:27 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -12,4 +12,20 @@ function take_snapshot(){
     });
 }
 console.log('ml5 version', ml5.version);
-classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/dyHF_gnS0/model.json',modelLoaded)
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/dyHF_gnS0/model.json',modelLoaded);
+function modelLoaded(){
+    console.log("modelo cargado");
+}
+function check(){
+    img = document.getElementById ('captured_image');
+    classifier.classify(img, gotResult);
+}
+function gotResult(error, results){
+    if(error){
+        console.log(error);
+    }else{
+        console.log(results);
+        document.getElementById("result_object_name").innerHTML = results[0].label;
+        document.getElementById("result_object_accuaracy").innerHTML = results[0].confidence.toFixed(3);
+    }
+}
